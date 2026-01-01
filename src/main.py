c8aeee18733a69db972080eba3f9717ec49f5385
shashank-suresh <shashanksuresh3@gmail.com>
2026-01-01 10:10:57 +0530

    Integrated rejection_rate_delta_baseline feature creation into flow

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from aggregations import add_warmup_indicator, compute_rejection_rate_metrics
+from features import compute_rejection_rate_baseline
 
 if __name__ == "__main__":
     df = pd.read_csv("data/events.csv")
@@ -8,4 +9,4 @@ if __name__ == "__main__":
     cp_df = compute_rejection_rate_metrics(df, "CP1")
     cp_df = add_warmup_indicator(cp_df)
 
-    print(cp_df["rejection_rate_30m"])
+    cp_df = compute_rejection_rate_baseline(cp_df)
