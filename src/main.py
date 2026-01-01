566a68b2970f44902ae9bd174624a5af3d9b5afd
shashank-suresh <shashanksuresh3@gmail.com>
2026-01-01 11:00:26 +0530

    Integrated anomaly score calculation into main workflow

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from aggregations import add_warmup_indicator, compute_rejection_rate_metrics
+from anomaly import calculate_anomaly_score
 from features import compute_rejection_rate_baseline
 
 if __name__ == "__main__":
@@ -10,3 +11,5 @@ if __name__ == "__main__":
     cp_df = add_warmup_indicator(cp_df)
 
     cp_df = compute_rejection_rate_baseline(cp_df)
+
+    cp_df = calculate_anomaly_score(cp_df)
